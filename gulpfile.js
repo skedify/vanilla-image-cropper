3050ecc13fbc235414ebdf835a68aea2b4383c10
ThomasB <thomas.buys45@gmail.com>
2018-10-22 22:53:00 +0200

    wip

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,13 @@
 const gulp = require('gulp')
-const runSequence = require('run-sequence')
 
 const sync = require('browser-sync').create()
 
-const rollup = require('gulp-rollup')
-const buble = require('rollup-plugin-buble')
+const rollup = require('rollup-stream')
+const babel = require('rollup-plugin-babel')
 const { eslint } = require('rollup-plugin-eslint')
 const resolve = require('rollup-plugin-node-resolve')
 const commonjs = require('rollup-plugin-commonjs')
-const uglify = require('rollup-plugin-uglify')
+const { uglify } = require('rollup-plugin-uglify')
 
 const rename = require('gulp-rename')
 const sass = require('gulp-sass')
@@ -59,8 +58,16 @@ function doRollup(component) {
             browser: true,
           }),
           commonjs(),
-          eslint('.eslintrc.yaml'),
-          buble(),
+          eslint('.eslintrc.yml'),
+          babel({
+            presets: [
+              'env',
+              {
+                modules: false,
+              },
+            ],
+            plugins: [],
+          }),
           uglify(),
         ],
       })
@@ -107,7 +114,7 @@ gulp.task('scss', () => doSCSS(config.scss).pipe(gulp.dest('docs/dist')))
 
 gulp.task('docs:js', () => doRollup(config.docs.js))
 gulp.task('docs:scss', () => doSCSS(config.docs.scss))
-gulp.task('docs', () => runSequence('docs:js', 'docs:scss'))
+gulp.task('docs', gulp.series('docs:js', 'docs:scss'))
 
 //
 //  BUILD TASKS
@@ -115,25 +122,28 @@ gulp.task('docs', () => runSequence('docs:js', 'docs:scss'))
 
 gulp.task('reload', () => sync.reload())
 
-gulp.task('serve', ['dist'], () => {
-  sync.init({
-    server: ['./docs', './dist'],
-    index: 'index.html',
-    files: [
-      './dist/js/imagecrop.min.js',
-      './dist/css/imagecrop.min.css',
-      './docs/index.html',
-      './docs/app.min.js',
-    ],
+gulp.task('dist', gulp.parallel('js', 'scss', 'docs'))
+
+gulp.task(
+  'serve',
+  gulp.series('dist', () => {
+    sync.init({
+      server: ['./docs', './dist'],
+      index: 'index.html',
+      files: [
+        './dist/js/imagecrop.min.js',
+        './dist/css/imagecrop.min.css',
+        './docs/index.html',
+        './docs/app.min.js',
+      ],
+    })
+
+    gulp.watch(config.scss.src, gulp.series('scss', 'reload'))
+    gulp.watch(config.js.src, gulp.series('js', 'reload'))
+    gulp.watch(config.docs.js.src, gulp.series('docs:js', 'reload'))
+    gulp.watch(config.docs.scss.src, gulp.series('docs:scss', 'reload'))
   })
-
-  gulp.watch(config.scss.src, () => runSequence('scss', 'reload'))
-  gulp.watch(config.js.src, () => runSequence('js', 'reload'))
-  gulp.watch(config.docs.js.src, () => runSequence('docs:js', 'reload'))
-  gulp.watch(config.docs.scss.src, () => runSequence('docs:scss', 'reload'))
-})
-
-gulp.task('dist', cb => runSequence('js', 'scss', 'docs', cb))
+)
 
 //  Wrapper task for building all files, and watching for changes.
-gulp.task('default', () => runSequence('serve'))
+gulp.task('default', gulp.series('serve'))
--- src/docs/app.js
+++ src/docs/app.js
@@ -4,49 +4,49 @@ import ImageCropper from '../js/imagecrop'
 //  Setup Image cropper
 
 new ImageCropper('.example-1', './example1.jpg', {
-    max_width: 300,
-    max_height: 300,
+  max_width: 300,
+  max_height: 300,
 })
 new ImageCropper('.example-2', './example2.jpg', {
-    max_width: 300,
-    max_height: 300,
+  max_width: 300,
+  max_height: 300,
 })
 
 //  Size examples
 
 new ImageCropper('.example-size-100', './example1.jpg', {
-    max_width: 100,
-    max_height: 100,
-    min_crop_width: 50,
-    min_crop_height: 50,
+  max_width: 100,
+  max_height: 100,
+  min_crop_width: 50,
+  min_crop_height: 50,
 })
 new ImageCropper('.example-size-200', './example1.jpg', {
-    max_width: 200,
-    max_height: 200,
-    min_crop_width: 50,
-    min_crop_height: 50,
+  max_width: 200,
+  max_height: 200,
+  min_crop_width: 50,
+  min_crop_height: 50,
 })
 new ImageCropper('.example-size-300', './example1.jpg', {
-    max_width: 300,
-    max_height: 300,
-    min_crop_width: 50,
-    min_crop_height: 50,
+  max_width: 300,
+  max_height: 300,
+  min_crop_width: 50,
+  min_crop_height: 50,
 })
 
 //  Mode example
 new ImageCropper('.example-mode-circular', './example2.jpg', {
-    max_width: 300,
-    max_height: 300,
-    mode: 'circular',
-    min_crop_width: 50,
-    min_crop_height: 50,
+  max_width: 300,
+  max_height: 300,
+  mode: 'circular',
+  min_crop_width: 50,
+  min_crop_height: 50,
 })
 
 //  Fixed
 new ImageCropper('.example-fixed-on', './example2.jpg', {
-    max_width: 300,
-    max_height: 300,
-    fixed_size: true,
-    min_crop_width: 50,
-    min_crop_height: 50,
+  max_width: 300,
+  max_height: 300,
+  fixed_size: true,
+  min_crop_width: 50,
+  min_crop_height: 50,
 })
